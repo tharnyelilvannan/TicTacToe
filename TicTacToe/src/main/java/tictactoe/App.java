c38ffea421ad2ed57554ce5bd558ee9eef9f10be
tharnyelilvannan <tharny2005@outlook.com>
2024-11-11 11:39:04 -0500

    Add classes

--- TicTacToe/src/main/java/tictactoe/App.java
+++ TicTacToe/src/main/java/tictactoe/App.java
@@ -1,10 +1,33 @@
 package tictactoe;
 
 /**
- * Hello world!
+ * Author: Tharny Elilvannan
+ * Last Updated: November 11, 2024
+ * Purpose: Tic Tac Toe game.
  */
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+
+        Game g = new Game();
+        
+    } // end of main method
+
+} // end of App class
+
+class Game {
+
+} // end of Game class
+
+class Grid {
+
+} // end of Grid class
+
+class ComputerPlayer {
+
+} // end of ComputerPlayer class
+
+class HumanPlayer {
+
+} // end of HumanPlayer class
